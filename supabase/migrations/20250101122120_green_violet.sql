b9165c4daccc5203e1735ec178c92f429d73b568
Wijet-io <hello.wijet@gmail.com>
2025-01-01 13:23:35 +0100

    add migration

---
+++ supabase/migrations/20250101122120_green_violet.sql
@@ -0,0 +1,12 @@
+@@ .. @@
+ -- Insert Jibble configuration
+ insert into settings (key, value)
+ values (
+   'jibble_config',
+   jsonb_build_object(
+-    'apiKey', '70ee9d49-920c-47a8-94fb-3e82bc0edaf0',
+-    'apiSecret', '2dlOwCgFItcutiLbthzlAkMUP6q6pjuaRsWyCT6cBBvly7pL'
++    'api_key', '70ee9d49-920c-47a8-94fb-3e82bc0edaf0',
++    'api_secret', '2dlOwCgFItcutiLbthzlAkMUP6q6pjuaRsWyCT6cBBvly7pL'
+   )
+ );
